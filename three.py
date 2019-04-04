c3499fd9d5f5d2809a68f1d7ad24afec8c0245fd
zk_mac_test <519715203@qq.com>
2019-04-04 14:49:12 +0800

    提交two.txt和three.py

---
+++ three.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+#   Copyright (C) 2019 MyCompany. All rights reserved.
+#   
+#   File Name  : three.py
+#   Author     : Zhaokui
+#   Create Date: 2019-04-04 14:47
+#   Describle  : 
+#
+# ==============================================================================
+
+
+def main():
+    print("I am a python file.")
+
+
+if __name__ == "__main__":
+    main()
+
